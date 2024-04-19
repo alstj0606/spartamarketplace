b475b2ab972c3b4f5c22d7d1680415d31d4ac6ae
alstj0606 <alstjwjddus0606@naver.com>
2024-04-19 16:29:05 +0900

    edit item

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
-    
-    
+
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
@@ -21,7 +20,7 @@ def new(request):
     if request.method == 'POST':
         form = NewItemForm(request.POST, request.FILES)
         
-        if form.is_valid():  # 수정된 부분
+        if form.is_valid():
             item = form.save(commit=False)
             item.created_by = request.user
             item.save()
@@ -32,12 +31,31 @@ def new(request):
     
     return render(request, 'item/form.html', {
         'form': form,
-        'title': 'New Item',
+        'title': 'New item',
+    })
+    
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+            
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
     
 @login_required
 def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
-    
+
     return redirect('dashboard:index')
